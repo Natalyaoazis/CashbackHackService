1b5038e344eaabf29161755b85a2415baabf957c
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2022-01-24 16:43:05 +0300

    TestNG

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
  public class CashbackHackServiceTest {
      CashbackHackService service = new CashbackHackService();
@@ -9,14 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
     public void shouldRemain() {
         int expected = 300;
         int actual = service.remain(700);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
      @Test
      public void shouldCheckRemainIfAmountEqualsBondary() {
          int expected = 0;
          int actual = service.remain(1000);
-         assertEquals(expected, actual);
+         assertEquals(actual, expected);
      }
 
      @Test
